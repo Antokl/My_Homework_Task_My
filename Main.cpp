bdb5140bde92107e640135288d6f2a49e9e6ffa4
Antokl <liantokha@mail.ru>
2024-09-18 11:41:18 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+
+enum class Result
+{
+	SUCCESS,
+	FAIL,
+	ERROR_WRITE,
+	ERROR_READ
+};
+
+enum class Another
+{
+	WORK,
+	SUCCESS
+};
+
+Result DoWork()
+{
+	return Result::SUCCESS;
+}
+
+enum Alphabet
+{
+	A,
+	B,
+	C
+};
+
+int main()
+{
+	std::cout << A;
+	Result workResult = DoWork();
+	if (workResult == Result::SUCCESS)
+	{
+
+	}
+	Another anotherRes = Another::WORK;
+	//if (workResult == anotherRes)
+	{
+
+	}
+}
