8196c633bfca369e3f7574ac48f6d507fd0cbb89
Antokl <liantokha@mail.ru>
2024-09-18 12:08:55 +0300

    student struc

--- Main.cpp
+++ Main.cpp
@@ -8,9 +8,16 @@ struct Student
 	int Age;
 	int Height;
 	std::string Name;
+
+	void GetInfo()
+	{
+		std::cout << "student struc";
+	}
 };
 
 int main()
 {
 	Student* ptr = new Student{ 10, 160, "Paul" };
+	ptr->GetInfo();
+
 }
